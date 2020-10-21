59b2d62a87dfca455e92936ac94caeaace3eb297
Daniel Browne <dsbrowne@ucsc.edu>
2020-10-20 20:53:45 -0500

    Coupon weeklyreward added

--- InfiniteMeals/Checkout/ServingFreshCoupon.cs
+++ InfiniteMeals/Checkout/ServingFreshCoupon.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 namespace InfiniteMeals.Checkout
 {
     public class Coupon
@@ -18,8 +22,71 @@ namespace InfiniteMeals.Checkout
         public string recurring { get; set; }
         public string email_id { get; set; }
         public string cup_business_uid { get; set; }
-    }
 
+
+        // Checks if the user associated with email needs to be awarded a Weekly Order Coupon
+        // Posts coupon if awarded
+        // Returns a task<TResult> indicating whether Post occured successfully (false not awarded/post failed)
+        public static async Task<bool> WeeklyOrderCoupon(string userEmail)
+        {
+            string historyEndpoint = "https://tsx3rnuidi.execute-api.us-west-1.amazonaws.com/dev/api/v2/history/";
+            string userEndpoint = String.Concat(historyEndpoint, userEmail);
+            // Open Http Connection
+            var client = new HttpClient();
+            // Get Data from endpoint
+            var RDSresponse = await client.GetAsync(userEndpoint);
+            var message = await RDSresponse.Content.ReadAsStringAsync();
+            System.Diagnostics.Debug.WriteLine(RDSresponse.IsSuccessStatusCode);
+            JObject history = JObject.Parse(message);               // Parse Object to get as JObject
+            JArray result = history["result"].Value<JArray>();      // Get useful info from Object
+            System.Diagnostics.Debug.WriteLine(result);
+            List<DateTime> orderDates = new List<DateTime>();
+            if (result.Count >= 5)                                  // five orders exists
+            {
+                foreach (JObject item in result.Children())         // put last 5 order dates into list
+                {
+                    string sDate = item["purchase_date"].Value<string>();
+                    DateTime date = DateTime.Parse(sDate);
+                    orderDates.Add(date);
+                }
+                System.Diagnostics.Debug.WriteLine(String.Join("\n", orderDates));
+                for (int i = 0; i < orderDates.Count - 2; i++)                  // look at pairs 0-1, 1-2, 2-3, 3-4
+                {
+                    TimeSpan interval = orderDates[i + 1] - orderDates[i];      // get interval between orders
+                    if (interval.Days > 7)                                       // any interval > 7 days
+                    {
+                        return false;
+                    }
+                }
+                // If method gets here Coupon should be awarded
+                // Set up coupon
+                Coupon weeklyOrderCoupon = new Coupon();
+                DateTime current = DateTime.Now;
+                current.AddMonths(3);
+                string expire = current.ToString();
+
+                weeklyOrderCoupon.coupon_uid = "";
+                weeklyOrderCoupon.coupon_id = "weeklyOrderCoupon";
+                weeklyOrderCoupon.cup_business_uid = "";
+                weeklyOrderCoupon.discount_amount = 0;
+                weeklyOrderCoupon.discount_percent = 15;
+                weeklyOrderCoupon.discount_shipping = 0;
+                weeklyOrderCoupon.email_id = userEmail;
+                weeklyOrderCoupon.expire_date = expire;
+                weeklyOrderCoupon.limits = 1;
+                weeklyOrderCoupon.notes = "";
+                weeklyOrderCoupon.num_used = 0;
+                weeklyOrderCoupon.recurring = "F";
+
+                var formattedCoupon = new StringContent(JsonConvert.SerializeObject(weeklyOrderCoupon), Encoding.UTF8, "application/json");
+                var httpResponse = await client.PostAsync(userEndpoint, formattedCoupon);
+                Console.WriteLine("This is the order's response " + httpResponse.Content.ReadAsStringAsync());
+
+                return httpResponse.IsSuccessStatusCode;
+            }
+            return false;
+        }
+    }
     public class Result
     {
         public string message { get; set; }
@@ -33,4 +100,4 @@ namespace InfiniteMeals.Checkout
         public string message { get; set; }
         public Result result { get; set; }
     }
-}
+    }
--- InfiniteMeals/profileUser.xaml.cs
+++ InfiniteMeals/profileUser.xaml.cs
@@ -1,41 +1,125 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
 using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using System.Json;
+using Coupon = InfiniteMeals.Checkout.Coupon;
+
+//using Stripe;
+
 
 namespace InfiniteMeals
-{
+{ 
+
     public partial class profileUser : ContentPage
     {
         static bool state = false;
+
+        // WeeklyPurchaseCoupon(string userEmail)
+        // Determines if user needs a weekly purchase coupon awarded
+        // Returns 1 if coupon was awarded, 0 if it was not, -1 if user already has coupon
+        //async public void WeeklyPurchaseCoupon(string userEmail)
+        //{
+        //    string historyEndpoint = "https://tsx3rnuidi.execute-api.us-west-1.amazonaws.com/dev/api/v2/history/";
+        //    string userEndpoint = String.Concat(historyEndpoint, userEmail);
+        //    // Open Http Connection
+        //    var client = new HttpClient();
+        //    // Get Data from endpoint
+        //    var RDSresponse = await client.GetAsync("https://tsx3rnuidi.execute-api.us-west-1.amazonaws.com/dev/api/v2/history/abc@gmail.com");
+        //    var message = await RDSresponse.Content.ReadAsStringAsync();
+        //    System.Diagnostics.Debug.WriteLine(RDSresponse.IsSuccessStatusCode);
+        //    JObject history = JObject.Parse(message);               // Parse Object to get as JObject
+        //    JArray result = history["result"].Value<JArray>();      // Get useful info from Object
+        //    System.Diagnostics.Debug.WriteLine(result);
+        //    List<DateTime> orderDates = new List<DateTime>();
+        //    if (result.Count >= 5)                                  // five orders exists
+        //    {
+        //        foreach (JObject item in result.Children())         // put last 5 order dates into list
+        //        {
+        //            string sDate = item["purchase_date"].Value<string>();
+        //            DateTime date = DateTime.Parse(sDate);
+        //            orderDates.Add(date);
+        //        }
+        //        System.Diagnostics.Debug.WriteLine(String.Join("\n", orderDates));
+        //        string giveCoupon = "true";
+        //        for (int i = 0; i < orderDates.Count - 2; i++)                  // look at pairs 0-1, 1-2, 2-3, 3-4
+        //        {
+        //            TimeSpan interval = orderDates[i + 1] - orderDates[i];      // get interval between orders
+        //            if (interval.Days > 7)                                       // any interval > 7 days
+        //            {
+        //                System.Diagnostics.Debug.WriteLine("Coupon was not earned");
+        //               giveCoupon = "false";                                     // This coupon is not awarded
+        //            }
+        //        }
+
+        //        Application.Current.Properties["weeklyPurchaseCoupon"] = giveCoupon;
+        //        // Post coupon to Coupons Endpoint
+
+        //    } 
+        //}
+
         public profileUser()
         {
             InitializeComponent();
 
-            userEmailAddress.Text = (string)Application.Current.Properties["userEmailAddress"];
-            userFirstName.Text = (string)Application.Current.Properties["userFirstName"];
-            userLastName.Text = (string)Application.Current.Properties["userLastName"];
+            if (Application.Current.Properties.ContainsKey("userEmailAddress"))     // Check userEmailAddress in dict
+            {
+                userEmailAddress.Text = (string)Application.Current.Properties["userEmailAddress"];
+            }
+            if (Application.Current.Properties.ContainsKey("userFirstName"))        // Check userFirstName in dict
+            {
+                userFirstName.Text = (string)Application.Current.Properties["userFirstName"];
+            }
+            if (Application.Current.Properties.ContainsKey("userLastName"))         // Check userLastName in dict
+            {
+                userLastName.Text = (string)Application.Current.Properties["userLastName"];
+            }
             userPassword.Text = "*******";
             userConfirmPassword.Text = "*******";
+            if (Application.Current.Properties.ContainsKey("userAddress"))          // Check userAddress in dict
+            {
+                userAddress.Text = (string)Application.Current.Properties["userAddress"];
+            }
+            if (Application.Current.Properties.ContainsKey("userAddressUnit"))      // Check userAddressUnit in dict
+            {
+                userUnitNumber.Text = (string)Application.Current.Properties["userAddressUnit"];
+            }
+            if (Application.Current.Properties.ContainsKey("userCity"))             // Check userCity in dict
+            {
+                userCity.Text = (string)Application.Current.Properties["userCity"];
+            }
+            if (Application.Current.Properties.ContainsKey("userState"))            // Check userState in dict
+            {
+                userState.Text = (string)Application.Current.Properties["userState"];
+            }
+            if (Application.Current.Properties.ContainsKey("userZipCode"))          // Check userZipCode in dict
+            {
+                userZipcode.Text = (string)Application.Current.Properties["userZipCode"];
+            }
+            if (Application.Current.Properties.ContainsKey("userPhoneNumber"))      // Check userPhoneNumber in dict
+            {
+                userPhoneNumber.Text = (string)Application.Current.Properties["userPhoneNumber"];
+            }
+            if (Application.Current.Properties.ContainsKey("latitude"))            // Check latitude in dict (assume longitude exists)
+            {
+                // only do map actions if a location can be found
+                Position position = new Position(Double.Parse(Application.Current.Properties["latitude"].ToString()), Double.Parse(Application.Current.Properties["longitude"].ToString()));
+                map.MapType = MapType.Street;
+                var mapSpan = new MapSpan(position, 0.001, 0.001);
 
-            userAddress.Text = (string)Application.Current.Properties["userAddress"];
-            userUnitNumber.Text = (string)Application.Current.Properties["userAddressUnit"];
-            userCity.Text = (string)Application.Current.Properties["userCity"];
-            userState.Text = (string)Application.Current.Properties["userState"];
-            userZipcode.Text = (string)Application.Current.Properties["userZipCode"];
-            userPhoneNumber.Text = (string)Application.Current.Properties["userPhoneNumber"];
+                Pin address = new Pin();
+                address.Label = "Delivery Address";
+                address.Type = PinType.SearchResult;
+                address.Position = position;
 
-            Position position = new Position(Double.Parse(Application.Current.Properties["latitude"].ToString()), Double.Parse(Application.Current.Properties["longitude"].ToString()));
-            map.MapType = MapType.Street;
-            var mapSpan = new MapSpan(position, 0.001, 0.001);
-
-            Pin address = new Pin();
-            address.Label = "Delivery Address";
-            address.Type = PinType.SearchResult;
-            address.Position = position;
-
-            map.MoveToRegion(mapSpan);
-            map.Pins.Add(address);
+                map.MoveToRegion(mapSpan);
+                map.Pins.Add(address);
+            }
+            
         }
 
         //void Button_Clicked(System.Object sender, System.EventArgs e)
@@ -99,6 +183,85 @@ namespace InfiniteMeals
         void SaveChangesClick(System.Object sender, System.EventArgs e)
         {
             
+        }
+
+        private async void RewardsClicked(object sender, EventArgs e)
+        {
+            // set top buttons to white and orange
+            DeliveryInfoButton.TextColor = Color.White;
+            RewardsButton.TextColor = Color.FromHex("#FF8500");
+
+            // Disable all info for reward view
+            userAddress.IsVisible = false;
+            userCity.IsVisible = false;
+            userConfirmPassword.IsVisible = false;
+            userEmailAddress.IsVisible = false;
+            userFirstName.IsVisible = false;
+            userLastName.IsVisible = false;
+            userPassword.IsVisible = false;
+            userPhoneNumber.IsVisible = false;
+            userState.IsVisible = false;
+            userUnitNumber.IsVisible = false;
+            userZipcode.IsVisible = false;
+            map.IsVisible = false;
+            pushNotificationEntry.IsVisible = false;
+            pushNotificationSwitch.IsVisible = false;
+            mapFrame.IsVisible = false;
+            saveChangesButton.IsVisible = false;
+
+            // set Rewards items to visible
+            reward1.IsVisible = true;
+            reward2.IsVisible = true;
+            reward3.IsVisible = true;
+            // Use Endpoint to get user data:
+            if (Application.Current.Properties.ContainsKey("userEmailAddress"))
+            {
+                string userEmailAddress = Application.Current.Properties["userEmailAddress"].ToString();
+                bool weeklyCoupon = Coupon.WeeklyOrderCoupon(userEmailAddress).Result;
+                if (weeklyCoupon)
+                {
+                    reward1.Text = "Coupon Available as a reward for weekly purchases";
+                }
+                else
+                {
+                    reward1.Text = "Weekly Purchase Coupon Unavalable";
+                }
+            }
+
+
+
+            //reward1.Text = String.Format("{0} / 9 orders completed for reward 1", orders);
+            //reward1PNG.Source = String.Format("Reward{0}.png", orders);
+        }
+
+        private void DeliveryInfoButton_Clicked(object sender, EventArgs e)
+        {
+            // set top buttons to white and orange
+            RewardsButton.TextColor = Color.White;
+            DeliveryInfoButton.TextColor = Color.FromHex("#FF8500");
+
+            // Disable all info for reward view
+            userAddress.IsVisible = true;
+            userCity.IsVisible = true;
+            userConfirmPassword.IsVisible = true;
+            userEmailAddress.IsVisible = true;
+            userFirstName.IsVisible = true;
+            userLastName.IsVisible = true;
+            userPassword.IsVisible = true;
+            userPhoneNumber.IsVisible = true;
+            userState.IsVisible = true;
+            userUnitNumber.IsVisible = true;
+            userZipcode.IsVisible = true;
+            map.IsVisible = true;
+            pushNotificationEntry.IsVisible = true;
+            pushNotificationSwitch.IsVisible = true;
+            mapFrame.IsVisible = true;
+            saveChangesButton.IsVisible = true;
+
+            // set Rewards items to visible
+            reward1.IsVisible = false;
+            reward2.IsVisible = false;
+            reward3.IsVisible = false;
         }
     }
 }
